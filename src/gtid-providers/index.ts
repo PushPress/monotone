ca221dbfd1011ccbcef1e3a9d976729d46a8f091
Ian Kaplan <ian.kaplan20@gmail.com>
2025-09-20 16:35:23 -0400

    add in memory map git provider

---
+++ src/gtid-providers/index.ts
@@ -0,0 +1,8 @@
+/**
+ * GTID Provider implementations
+ * 
+ * This module contains various implementations of the GTIDProvider interface,
+ * each optimized for different use cases and storage strategies.
+ */
+
+export * from './map-gtid-provider.js';
